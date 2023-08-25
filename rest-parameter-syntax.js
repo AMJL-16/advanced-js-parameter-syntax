3184334a15aa381943fe96d39c3b521467355ff5
AMJL-16 <ozaurelio@gmail.com>
2023-08-25 14:21:36 +0000

    Added new content to rest parameter

--- rest-parameter-syntax.js
+++ rest-parameter-syntax.js
@@ -3,8 +3,46 @@
  * command node rest-parameter-syntax.js in the terminal
  */
 
-// Regular function call 
+/*The rest parameter syntax works by combining individual element
+into an array.
+it can also be used to represent the rest of the values 
+when destructuring an array or object */ 
 
-// Extra arguments are ignored
+// Regular function call 
+const sumAll = (a, b, c) => a + b + c;
+let sum = sumAll(1,2,3);
+console.log('sum:', sum);
+
+// Extra arguments are ignored, no rest parameter
+let sum2 = sumAll(1, 2, 3, 4, 5, 6);
+console.log('sum2:', sum2);// not what we want, so let's use the rest parameter
 
 // Function using ...rest
+const sumRest = (a, b, c, ...rest) => {
+  let sum = a + b + c; // sum of the 3 first variables, easy enough but what about the rest of num...
+  for (let i of rest) {
+    sum += i;
+  }
+  return sum;
+}
+
+let sum3 = sumRest(1, 2, 3, 4, 5, 6);
+console.log(sum3);
+
+// challenge on rest parameter syntax
+
+let mixedLetters = ['b', 'd', 'a', 'c', 'f', 'e'];
+
+let moreMixedLetters = [...mixedLetters, 'h', 'k', 'g', 'j', 'i', 'l'];
+console.log(moreMixedLetters);
+
+
+const updateSortReverse = (arr, ...letters) => {
+    let allLetters = [...arr, ...letters];
+    return allLetters.sort().reverse();
+};
+
+let reverseSort = updateSortReverse(moreMixedLetters, 'n', 'm', 'o');
+console.log(reverseSort);
+console.log(mixedLetters);
+
